3f1b2c25098f3b2c1a8284ad3c5d8a7c0e905f8a
mainibet <mainibet@gmail.com>
2025-12-02 09:03:30 +0100

    testing possible improvements in move.c not sure yet. It was better with defines

--- src/movement/move.c
+++ src/movement/move.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 07:49:16 by albetanc          #+#    #+#             */
-/*   Updated: 2025/12/01 10:57:14 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/12/02 08:57:28 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,14 @@ void	move_forward(t_game *game)
 
 	nx = game->player.x + game->player.dir_x * game->player.move_speed;
 	ny = game->player.y + game->player.dir_y * game->player.move_speed;
-	if (game->player.dir_x > 0)
-	{
-		if (game->map.map[(int)game->player.y][(int)nx] != '1')
-			game->player.x = nx;
-	}
-	else
-	{
-		if (game->map.map[(int)game->player.y][(int)(nx - COLLI_DIST)] != '1')
-			game->player.x = nx;
-	}
-	if (game->player.dir_y > 0)
-	{
-		if (game->map.map[(int)ny][(int)game->player.x] != '1')
-			game->player.y = ny;
-	}
-	else
-	{
-		if (game->map.map[(int)(ny - COLLI_DIST)][(int)game->player.x] != '1')
-			game->player.y = ny;
-	}
+	//if (game->map.map[(int)game->player.y][(int)nx] != '1')
+	//	game->player.x = nx;
+	//if (game->map.map[(int)ny][(int)game->player.x] != '1')
+	//	game->player.y = ny;
+	if (game->map.map[(int)game->player.y][(int)(nx + game->player.dir_x * COLLI_DIST)] != '1')
+		game->player.x = nx;
+	if (game->map.map[(int)(ny + game->player.dir_y * COLLI_DIST)][(int)game->player.x] != '1')
+		game->player.y = ny;
 }
 
 void	move_back(t_game *game)
@@ -52,26 +40,10 @@ void	move_back(t_game *game)
 	//	game->player.x = nx;
 	//if (game->map.map[(int)ny][(int)game->player.x] != '1')
 	//	game->player.y = ny;
-	if (game->player.dir_x > 0)
-	{
-		if (game->map.map[(int)game->player.y][(int)(nx + COLLI_DIST)] != '1')
-			game->player.x = nx;
-	}
-	else
-	{
-		if (game->map.map[(int)game->player.y][(int)(nx - COLLI_DIST)] != '1')
-			game->player.x = nx;
-	}
-	if (game->player.dir_y > 0)
-	{
-		if (game->map.map[(int)(ny + COLLI_DIST)][(int)game->player.x] != '1')
-			game->player.y = ny;
-	}
-	else
-	{
-		if (game->map.map[(int)(ny - COLLI_DIST)][(int)game->player.x] != '1')
-			game->player.y = ny;
-	}
+	if (game->map.map[(int)game->player.y][(int)(nx - game->player.dir_x * COLLI_DIST)] != '1') // <--- Fíjate en el signo menos
+		game->player.x = nx;
+	if (game->map.map[(int)(ny - game->player.dir_y * COLLI_DIST)][(int)game->player.x] != '1') // <--- Fíjate en el signo menos
+		game->player.y = ny;
 }
 
 void	move_left(t_game *game)
@@ -85,26 +57,10 @@ void	move_left(t_game *game)
 	//	game->player.x = nx;
 	//if (game->map.map[(int)ny][(int)game->player.x] != '1')
 	//	game->player.y = ny;
-	if (game->player.plane_x > 0)
-	{
-		if (game->map.map[(int)game->player.y][(int)(nx + COLLI_DIST)] != '1')
-			game->player.x = nx;
-	}
-	else
-	{
-		if (game->map.map[(int)game->player.y][(int)(nx - COLLI_DIST)] != '1')
-			game->player.x = nx;
-	}
-	if (game->player.plane_y > 0)
-	{
-		if (game->map.map[(int)(ny + COLLI_DIST)][(int)game->player.x] != '1')
-			game->player.y = ny;
-	}
-	else
-	{
-		if (game->map.map[(int)(ny - COLLI_DIST)][(int)game->player.x] != '1')
-			game->player.y = ny;
-	}
+	if (game->map.map[(int)game->player.y][(int)(nx - game->player.plane_x * COLLI_DIST)] != '1')
+		game->player.x = nx;
+	if (game->map.map[(int)(ny - game->player.plane_y * COLLI_DIST)][(int)game->player.x] != '1')
+		game->player.y = ny;
 }
 
 void	move_right(t_game *game)
@@ -118,26 +74,10 @@ void	move_right(t_game *game)
 	//	game->player.x = nx;
 	//if (game->map.map[(int)ny][(int)game->player.x] != '1')
 	//	game->player.y = ny;
-	if (game->player.plane_x > 0)
-	{
-		if (game->map.map[(int)game->player.y][(int)(nx + COLLI_DIST)] != '1')
-			game->player.x = nx;
-	}
-	else
-	{
-		if (game->map.map[(int)game->player.y][(int)(nx - COLLI_DIST)] != '1')
-			game->player.x = nx;
-	}
-	if (game->player.plane_y > 0)
-	{
-		if (game->map.map[(int)(ny + COLLI_DIST)][(int)game->player.x] != '1')
-			game->player.y = ny;
-	}
-	else
-	{
-		if (game->map.map[(int)(ny - COLLI_DIST)][(int)game->player.x] != '1')
-			game->player.y = ny;
-	}
+	if (game->map.map[(int)game->player.y][(int)(nx + game->player.plane_x * COLLI_DIST)] != '1')
+		game->player.x = nx;
+	if (game->map.map[(int)(ny + game->player.plane_y * COLLI_DIST)][(int)game->player.x] != '1')
+		game->player.y = ny;
 }
 
 void	handle_movement(t_game *game)
