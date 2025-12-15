b026755b64d93432269546eb0c4d2a7402cd7942
mainibet <mainibet@gmail.com>
2025-12-15 14:30:20 +0100

    Fix(render_wall.c): fixed north and east walls

--- src/render/render_wall.c
+++ src/render/render_wall.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 15:03:16 by albetanc          #+#    #+#             */
-/*   Updated: 2025/12/12 20:35:37 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/12/15 14:29:40 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,11 @@ void	get_wall_sampler(t_sampler *sam, t_game *game, t_ray *ray, t_wall wall)
 	hit = ray->hit;
 	if (hit >= 1.0)
 		hit = 1.0 - EPSILON;
+	//sam->tex_x = (int)(hit * wall.tex->width);
+	//if ((ray->side == 0 && ray->step_x > 0)
+	//	|| (ray->side == 1 && ray->step_y < 0))
+	//sam->tex_x = wall.tex->width - sam->tex_x - 1;
 	sam->tex_x = (int)(hit * wall.tex->width);
-	if ((ray->side == 0 && ray->step_x > 0)
-		|| (ray->side == 1 && ray->step_y < 0))
-		sam->tex_x = wall.tex->width - sam->tex_x - 1;
 	r_wall_hei = (double)game->display.win_h / ray->wall_dist;
 	sam->step = (double)wall.tex->height / r_wall_hei;
 	draw_start = game->display.win_h / 2.0 - r_wall_hei / 2.0;
@@ -75,6 +76,12 @@ static void	draw_col(t_game *game, t_wall data, t_ray *ray)
 	t_sampler	sam;
 
 	get_wall_sampler(&sam, game, ray, data);
+	printf("TEX_X=%d, hit=%.3f, side=%d, tex_name=%s\n", 
+           sam.tex_x, ray->hit, ray->side, 
+           (ray->side==1 && ray->step_y<0) ? "NORTH" : 
+           (ray->side==1 && ray->step_y>0) ? "SOUTH" : 
+           (ray->side==0 && ray->step_x>0) ? "EAST" : "WEST");
+    
 	y = 0;
 	while (y < game->display.win_h)
 	{
