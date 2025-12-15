0c010a055c7d2d77db38b358ba306481eac34b96
mainibet <mainibet@gmail.com>
2025-12-15 14:51:29 +0100

    Fix(render_wall.c): fixed texture orientation

--- src/render/render_wall.c
+++ src/render/render_wall.c
@@ -6,7 +6,7 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 15:03:16 by albetanc          #+#    #+#             */
-/*   Updated: 2025/12/15 14:29:40 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/12/15 14:50:53 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,10 @@ void	get_wall_sampler(t_sampler *sam, t_game *game, t_ray *ray, t_wall wall)
 	hit = ray->hit;
 	if (hit >= 1.0)
 		hit = 1.0 - EPSILON;
-	//sam->tex_x = (int)(hit * wall.tex->width);
-	//if ((ray->side == 0 && ray->step_x > 0)
-	//	|| (ray->side == 1 && ray->step_y < 0))
-	//sam->tex_x = wall.tex->width - sam->tex_x - 1;
 	sam->tex_x = (int)(hit * wall.tex->width);
+	if ((ray->side == 0 && ray->step_x < 0)
+		|| (ray->side == 1 && ray->step_y > 0))
+		sam->tex_x = wall.tex->width - sam->tex_x - 1;
 	r_wall_hei = (double)game->display.win_h / ray->wall_dist;
 	sam->step = (double)wall.tex->height / r_wall_hei;
 	draw_start = game->display.win_h / 2.0 - r_wall_hei / 2.0;
