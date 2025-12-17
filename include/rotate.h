ac935ee95c0a24aa1a5edd06b92c7a973179b7b0
mainibet <mainibet@gmail.com>
2025-12-17 17:51:29 +0100

    rotate.h norm happy

--- include/rotate.h
+++ include/rotate.h
@@ -6,14 +6,14 @@
 /*   By: albetanc <albetanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 17:30:38 by albetanc          #+#    #+#             */
-/*   Updated: 2025/12/17 17:42:05 by albetanc         ###   ########.fr       */
+/*   Updated: 2025/12/17 17:49:26 by albetanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ROTATE_H
-#define ROTATE_H
+# define ROTATE_H
 
-#include "cub.h"
+# include "cub.h"
 
 void	rotate_right(t_game *game);
 void	rotate_left(t_game *game);
